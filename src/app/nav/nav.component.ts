dfad35f0c8a4e2b57adc1f25c5c4db29ae9b5852
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-27 11:32:36 +0200

    refactor ModalService to handle multiple modals concurrently and independently

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -4,14 +4,14 @@ import { ModalService } from '../services/modal.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
- constructor(public modal: ModalService) { }
+  constructor(public modal: ModalService) {}
 
- openModal($event: Event) {
-  $event.preventDefault();
+  openModal($event: Event) {
+    $event.preventDefault();
 
-  this.modal.toggleModal();
- }
+    this.modal.toggleModal('auth');
+  }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -7,9 +7,11 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
+  @Input() modalID = '';
+
   constructor(public modal: ModalService) {}
 
   closeModal() {
-    this.modal.toggleModal();
+    this.modal.toggleModal(this.modalID);
   }
 }
