1a099d33e66015192365e507133b28bedf0940a3
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-27 16:03:40 +0200

    loop through tabs to render them

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>;
 
+  ngAfterContentInit(): void {
+    console.log(this.tabs)
+  }
 }
