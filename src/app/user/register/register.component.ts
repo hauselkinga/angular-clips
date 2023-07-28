64bcda439291cd4e141e348f6200e7adb509387b
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-28 18:17:37 +0200

    add password validation with regex pattern
    confirm password not implemented properly yet

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -21,8 +21,13 @@ export class RegisterComponent {
       Validators.min(18),
       Validators.max(120)
     ]),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+    ]),
+    confirm_password: new FormControl('', [
+      Validators.required
+    ]),
     phoneNumber: new FormControl('')
   })
 }
